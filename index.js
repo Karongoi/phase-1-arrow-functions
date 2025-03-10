d405cc18f0369601c2a901ed2506143c5b7559b9
dan <danielkarongoi@gmail.com>
2025-03-10 06:07:48 +0300

    added changes

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function expression for divide
+const divide = function (num) {
+    return num / 100;
+  };
+  
+  // Arrow function for square
+  const square = (num) => num * num;
+  
+  // Your existing add function
+  const add = (a, b) => a + b;
+  
